45189b03c61cf39a543ea14854a5bfd80a93adb0
idiz1114 <194805590+eunsuh-park@users.noreply.github.com>
2026-03-04 17:42:18 +0900

    노트 디테일 페이지 디자인 수정

--- src/pages/NoteDetail.js
+++ src/pages/NoteDetail.js
@@ -10,6 +10,11 @@ import '../components/NoteDetail.css';
 const PDFJS_CDN = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.min.js';
 const PDFJS_WORKER_CDN =
   'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js';
+
+/** NoteDetail PDF 로딩 오버레이용 Lottie */
+const NOTE_DETAIL_LOADING_LOTTIE =
+  'https://lottie.host/ac9f0d95-b144-482c-a2d4-fb707e069f94/lHcmDqwHwt.lottie';
+
 const ICONS = {
   arrowsLeftLine:
     "<svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 24 24'><title>arrows_left_line</title><g id='arrows_left_line' fill='none' fill-rule='evenodd'><path d='M24 0v24H0V0h24ZM12.594 23.258l-.012.002-.071.035-.02.004-.014-.004-.071-.036c-.01-.003-.019 0-.024.006l-.004.01-.017.428.005.02.01.013.104.074.015.004.012-.004.104-.074.012-.016.004-.017-.017-.427c-.002-.01-.009-.017-.016-.018Zm.264-.113-.014.002-.184.093-.01.01-.003.011.018.43.005.012.008.008.201.092c.012.004.023 0 .029-.008l.004-.014-.034-.614c-.003-.012-.01-.02-.02-.022Zm-.715.002a.023.023 0 0 0-.027.006l-.006.014-.034.614c0 .012.007.02.017.024l.015-.002.201-.093.01-.008.003-.011.018-.43-.003-.012-.01-.01-.184-.092Z'/><path fill='currentColor' d='M11.707 6.293a1 1 0 0 1 0 1.414L7.414 12l4.293 4.293a1 1 0 0 1-1.414 1.414l-5-5a1 1 0 0 1 0-1.414l5-5a1 1 0 0 1 1.414 0Zm6 0a1 1 0 0 1 0 1.414L13.414 12l4.293 4.293a1 1 0 0 1-1.414 1.414l-5-5a1 1 0 0 1 0-1.414l5-5a1 1 0 0 1 1.414 0Z'/></g></svg>",
@@ -98,7 +103,7 @@ export function renderNotePdfViewer(targetEl, id, options = {}) {
         </div>
         <canvas id="pdf-canvas"></canvas>
         <div id="pdf-overlay" class="pdf-overlay show">
-          <div class="pdf-spinner" aria-hidden="true"></div>
+          <dotlottie-wc class="pdf-overlay-lottie" src="${NOTE_DETAIL_LOADING_LOTTIE}" style="width: 300px; height: 300px" autoplay loop></dotlottie-wc>
           <div id="pdf-overlay-text">PDF 목록 불러오는 중...</div>
         </div>
       </div>
